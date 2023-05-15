991561f521ac2d5fa5b1fb9d6444b4549486f25b
Andrés Morales <amoralesma@unal.edu.co>
2023-05-14 22:02:38 -0500

    [Update] Images

--- src/px_robot/scripts/jointPub.py
+++ src/px_robot/scripts/jointPub.py
@@ -7,28 +7,27 @@ def joint_publisher():
     pub = rospy.Publisher('/joint_trajectory', JointTrajectory, queue_size=0)
     rospy.init_node('joint_publisher', anonymous=False)
     
+    trajs = [
+        [0, 0, 0, 0, 0], 
+        [0.2,0.4,0.7,0.1,0],
+         [-0.7, 0,0.2, -1.4, 0],
+        [0.8, 0.9, -0.6, 0.4, 0],
+        [0, 0, 1.5, 3/4, 0]
+    ]
     while not rospy.is_shutdown():
-        state = JointTrajectory()
-        state.header.stamp = rospy.Time.now()
-        state.joint_names = ["joint_1", "joint_2", "joint_3", "joint_4", "joint_5"]
-        point = JointTrajectoryPoint()
-        point.positions = [0, 0, 0, 0, 0]    
-        point.time_from_start = rospy.Duration(0.5)
-        state.points.append(point)
-        pub.publish(state)
-        print('published command')
-        rospy.sleep(1)
+        for q in trajs:
+            state = JointTrajectory()
+            state.header.stamp = rospy.Time.now()
+            state.joint_names = ["joint_1", "joint_2", "joint_3", "joint_4", "joint_5"]
+            point = JointTrajectoryPoint()
+            point.positions = q
+            point.time_from_start = rospy.Duration(0.9)
+            state.points.append(point)
+            pub.publish(state)
+            print('published command')
+            rospy.sleep(1.2)
 
-        state = JointTrajectory()
-        state.header.stamp = rospy.Time.now()
-        state.joint_names = ["joint_1", "joint_2", "joint_3", "joint_4", "joint_5"]
-        point = JointTrajectoryPoint()
-        point.positions = [0.25, 0, 0, 0, 1.3]    
-        point.time_from_start = rospy.Duration(0.5)
-        state.points.append(point)
-        pub.publish(state)
-        print('published command')
-        rospy.sleep(1)
+
 
 if __name__ == '__main__':
     try:
